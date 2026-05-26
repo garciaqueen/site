a2ba0dd14b3528004609cdfb19f75cde12a1016a
garciaqueen <mariastetsiuc1234@gmail.com>
2026-05-26 22:12:03 +0300

    initial commit

---
+++ src/main/java/org/example/auth/AccountManager.java
@@ -0,0 +1,68 @@
+package auth;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.example.database.DatabaseConnection;
+import org.example.auth.Account;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccountManager {
+
+    DatabaseConnection connection;
+
+    public AccountManager(DatabaseConnection connection) {
+        this.connection = connection;
+    }
+
+    public int register(String name, String password) throws SQLException {
+        PreparedStatement statement = connection.getConnection()
+                .prepareStatement("SELECT * FROM users WHERE name = ?;");
+        statement.setString(1, name);
+
+        statement.execute();
+        if (statement.getResultSet().next())
+            throw new RuntimeException("User " + name + " already exists.");
+
+        statement = connection.getConnection().prepareStatement("INSERT INTO users(name, password) VALUES (?, ?);");
+        statement.setString(1, name);
+        statement.setString(2, BCrypt.withDefaults().hashToString(12, password.toCharArray()));
+        statement.executeUpdate();
+
+        ResultSet resultSet = statement.getGeneratedKeys();
+        resultSet.next();
+        return resultSet.getInt(1);
+    }
+
+    public boolean authenticate(String name, String password) throws SQLException {
+        PreparedStatement statement = connection.getConnection()
+                .prepareStatement("SELECT password FROM users WHERE name = ?;");
+
+        statement.setString(1, name);
+        statement.execute();
+
+        ResultSet result = statement.getResultSet();
+        if (result.next()) {
+            String hashedPassword = result.getString("password");
+            BCrypt.Result cryptResult = BCrypt.verifyer().verify(password.toCharArray(), hashedPassword);
+            return cryptResult.verified;
+        } else
+            throw new RuntimeException("No such user " + name);
+    }
+
+    public Account getAccount(String name) throws SQLException {
+        PreparedStatement statement = connection.getConnection()
+                .prepareStatement("SELECT id, name FROM users WHERE name = ?;");
+        statement.setString(1, name);
+        statement.execute();
+
+        ResultSet result = statement.getResultSet();
+        if (result.next()) {
+            int id = result.getInt("id");
+            return new Account(id, name);
+        }
+        throw new RuntimeException("No such user " + name);
+    }
+
+}
